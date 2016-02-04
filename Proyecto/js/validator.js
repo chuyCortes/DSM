0128d4bd839a99bc2297037217f96ddb4ba44e9b
Cortes <chuy_2414@hotmail.es>
2016-02-03 18:07:30 -0600

    add text resize

--- Proyecto/js/validator.js
+++ Proyecto/js/validator.js
@@ -1,8 +1,64 @@
-$( document ).ready(function() {
-    
-	 var VarPreNoticia1  = $(".PreNoticia1 p").text(); 
-	 var valor =LimitarCampos(VarPreNoticia1,115);
+$( window ).resize(function() {
+  console.log("rez");
+  var w = window.innerWidth; 
+    var limite;
+	 
+	 if(w <= 349 )
+	 {
+	 	limite =100;
+	 }
+	 else if(w <= 768)
+	 {
+	 	limite =100
+	 }
+	 else if( w <=1264 )
+	 {
+	 	limite =220;
+	 }
+	 else if(w <= 1680)
+	 {
+	 	limite =300
+	 }
+
+	 var VarPreNoticia1  = $(".PreNoticia1 p").text();
+	 var valor =LimitarCampos(VarPreNoticia1,limite);
 	 $(".PreNoticia1 p").text(valor);
+});
+
+$( document ).ready(function() {
+    var w = window.innerWidth; 
+    var limite;
+	 console.log(w);
+	 if(w <= 349 )
+	 {
+	 	limite =100;
+	 }
+	 else if(w <= 768)
+	 {
+	 	limite =100
+	 }
+	 else if( w <=1264 )
+	 {
+	 	limite =220;
+	 }
+	 else if(w <= 1680)
+	 {
+	 	limite =300
+	 }
+
+	 var VarPreNoticia1  = $(".PreNoticia1 p").text();
+	 var valor =LimitarCampos(VarPreNoticia1,limite);
+	 $(".PreNoticia1 p").text(valor);
+
+	 var varNoticia2desc = $(".Noticia2desc p").text();
+	 valor = LimitarCampos(varNoticia2desc, 200);
+	 $(".Noticia2desc p").text(valor);
+
+	 var VarDescSabiasQue =$(".DescSabiasQue p").text();
+	 var valor =LimitarCampos(VarDescSabiasQue,limite);
+	 $(".DescSabiasQue p").text(valor);
+
+
 });
 
 
